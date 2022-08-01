20e9875829ac0ebc21f29d232e6c6f91a6b1d27f
IlyaTaidi <65737081+IlyaTaidi@users.noreply.github.com>
2022-08-01 16:45:10 -0400

    S1 updates

---
+++ src/components/MyTypewriter.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Typewriter from 'svelte-typewriter';
+  import type { TypewriterProps } from 'svelte-typewriter/types/typewriter.d';
+
+  const typewriterContent = [
+    'Artist',
+    'Developer',
+    'Designer',
+    'Musician',
+    'Maker',
+  ];
+  const typewriterProps = {
+    mode: 'loopRandom',
+    interval: 150,
+    delay: 100,
+    unwriteInterval: 150,
+  } as TypewriterProps;
+</script>
+
+<Typewriter {...typewriterProps}>
+  {#each typewriterContent as position}
+    <p class="typewriter">{position}</p>
+  {/each}
+</Typewriter>
+
+<style lang="scss">
+  @import '../variables.scss';
+  .typewriter {
+    font-weight: 600;
+    color: $teal;
+    font-size: $largeFont;
+    @media (min-aspect-ratio: 11/8) and (max-aspect-ratio: 32/15) {
+      margin-left: 0.3em;
+    }
+  }
+</style>
---
+++ src/pages/Hero.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+  import MyTypewriter from '../components/MyTypewriter.svelte';
+
+  const namedGradient = {
+    src: './src/assets/gradientv3.png',
+    name: 'Ilya Gnezdilov',
+    alt: 'Ilya Gnezdilov',
+    class: 'namedGradient',
+  };
+</script>
+
+<div class="S1">
+  <div class="Hero">
+    <p class="nameText">Hi, my name is</p>
+    <div class="heroImage">
+      <img {...namedGradient} alt="Ilya" />
+    </div>
+    <div class="typewriter">
+      <p>I want to be your next</p>
+      <MyTypewriter />
+    </div>
+    <div class="heroSubtext">
+      I’m a full stack developer specializing in designing and building
+      <span class="exceptional">exceptional</span> user experiences.
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  @import '../variables.scss';
+  .S1 {
+    display: grid;
+    grid-auto-flow: row;
+    grid-template-areas:
+      '. . .'
+      '. Hero .'
+      '. . .';
+    height: 100vh;
+    @media (min-aspect-ratio: 11/8) and (max-aspect-ratio: 32/15) {
+      grid-template-columns: 0.9fr 1.2fr 0.9fr;
+      grid-template-rows: 0.9fr 1.2fr 0.9fr;
+    }
+    @media (min-aspect-ratio: 32/15), (max-aspect-ratio: 11/8) {
+      grid-template-columns: auto;
+      grid-template-rows: 1fr auto 1fr;
+    }
+  }
+
+  .Hero {
+    display: grid;
+    grid-template-columns: 0.1fr 2.6fr 0.1fr;
+    gap: 3vh 0px;
+    grid-auto-rows: max-content;
+    height: max-content;
+    grid-template-areas:
+      '. nameText .'
+      '. heroImage .'
+      '. typewriter .'
+      '. heroSubtext .';
+    grid-area: Hero;
+  }
+
+  .nameText {
+    grid-area: nameText;
+    font-weight: 100;
+    align-self: end;
+    color: $blue;
+    font-size: $smallFont;
+  }
+
+  .heroSubtext {
+    grid-area: heroSubtext;
+    color: $darkBlue;
+    font-weight: 100;
+    font-size: $smallFont;
+    .exceptional {
+      color: $pink;
+    }
+  }
+
+  .heroImage {
+    grid-area: heroImage;
+  }
+
+  .typewriter {
+    grid-area: typewriter;
+    font-weight: 600;
+    color: $darkBlue;
+    font-size: $largeFont;
+    @media (min-aspect-ratio: 11/8) and (max-aspect-ratio: 32/15) {
+      display: flex;
+    }
+    @media (min-aspect-ratio: 32/15), (max-aspect-ratio: 11/8) {
+      display: block;
+    }
+  }
+  .namedGradient {
+    max-width: 91vw;
+  }
+</style>
