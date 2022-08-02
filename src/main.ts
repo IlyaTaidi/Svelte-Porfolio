f53377e5847406c1e6cd83c9774eafa7e2b263ca
IlyaTaidi <65737081+IlyaTaidi@users.noreply.github.com>
2022-08-02 12:48:57 -0400

    svg fill animation

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import App from './App.svelte'
 import "./variables.scss";
 
 const app = new App({
-  target: document.getElementById('app')
+  target: document.getElementById('app'),
+  props: {
+    name: 'world'
+  },
+  intro: true
 })
 
 export default app
