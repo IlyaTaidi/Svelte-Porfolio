f7682a2c8d9a370ded9cc0f8fc74d6bdea338b0e
IlyaTaidi <65737081+IlyaTaidi@users.noreply.github.com>
2022-08-02 15:53:37 -0400

    created animated navbar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import Loading from './components/Loading.svelte';
+  import Navbar from './components/Navbar.svelte';
   import Hero from './pages/Hero.svelte';
 </script>
 
+<Navbar />
 <Loading />
 <Hero />
