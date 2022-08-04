a4b851f3402bfed0d36a7df39e72f568f0eb3498
IlyaTaidi <65737081+IlyaTaidi@users.noreply.github.com>
2022-08-04 15:04:30 -0400

    S2 card styling

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -12,9 +12,17 @@
 <div class="S2">
   <div class="S2Card">
     <div class="S2Buttons">
-      <p on:click={(e) => handler(e)}>ABOUT</p>
-      <p on:click={(e) => handler(e)}>EXPERIENCE</p>
-      <p on:click={(e) => handler(e)}>CONTACT</p>
+      <p on:click={(e) => handler(e)} class:selected={cardState === 'ABOUT'}>
+        ABOUT
+      </p>
+      <p
+        on:click={(e) => handler(e)}
+        class:selected={cardState === 'EXPERIENCE'}>
+        EXPERIENCE
+      </p>
+      <p on:click={(e) => handler(e)} class:selected={cardState === 'CONTACT'}>
+        CONTACT
+      </p>
     </div>
     <div class="S2Main">
       <div class="S2Header">
@@ -26,7 +34,20 @@
         <h2 class="S2Job">Full Stack Designer</h2>
       </div>
       {#if cardState === 'ABOUT'}
-        <div class="S2About" transition:fade>About</div>
+        <div class="S2About" transition:fade>
+          <p class="AboutHeader">ABOUT</p>
+          <p class="AboutContent">
+            I’m a self taught Full Stack Developer with experience in JS/TS,
+            Python, Angular/Ionic, Cordova/Capacitor with a passion for Design.
+            Living in Florida, born in Ukraine.
+            <br />
+            <br />
+            Whether it’s creating servers in Node, Building complex database structures
+            in Firebase, Designing/Building beautiful PWA’s or creating unique user
+            experiences; I will bring your ideas to life.
+          </p>
+          <div class="AboutSocials">Socials</div>
+        </div>
       {/if}
       {#if cardState === 'EXPERIENCE'}
         <div class="S2Experience" transition:fade>Experience</div>
@@ -43,47 +64,92 @@
   .S2 {
     display: grid;
     height: 100vh;
-    grid-template-columns: 0.5fr 2fr 0.5fr;
-    grid-template-rows: 0.5fr 1.9fr 0.6fr;
-    gap: 0px 0px;
-    grid-auto-flow: row;
-    grid-template-areas:
-      '. . .'
-      '. S2Card .'
-      '. . .';
+    @media (min-aspect-ratio: 11/8) and (max-aspect-ratio: 32/15) {
+      grid-template-columns: 0.5fr 2fr 0.5fr;
+      grid-template-rows: 0.5fr 1.9fr 0.6fr;
+      gap: 0px 0px;
+      grid-auto-flow: row;
+      grid-template-areas:
+        '. . .'
+        '. S2Card .'
+        '. . .';
+    }
+    @media (min-aspect-ratio: 32/15), (max-aspect-ratio: 11/8) {
+      grid-template-columns: 0.2fr 2.6fr 0.2fr;
+      grid-template-rows: 0.2fr 2.6fr 0.2fr;
+      gap: 0px 0px;
+      grid-template-areas:
+        '. . .'
+        '. S2Card .'
+        '. . .';
+    }
   }
 
   .S2Card {
     display: grid;
-    grid-template-columns: auto 1.8fr;
-    grid-template-rows: 1fr;
-    gap: 0px 0px;
-    grid-auto-flow: row;
-    grid-template-areas: 'S2Buttons S2Main';
     grid-area: S2Card;
-    background: #ffffff8c;
+    color: $evenLighterBlue;
+    border-bottom: 1px solid $purple;
+    @media (min-aspect-ratio: 11/8) and (max-aspect-ratio: 32/15) {
+      grid-template-columns: auto 1.8fr;
+      grid-template-rows: 1fr;
+      grid-auto-flow: row;
+      grid-template-areas: 'S2Buttons S2Main';
+    }
+    @media (min-aspect-ratio: 32/15), (max-aspect-ratio: 11/8) {
+      grid-template-columns: 1fr;
+      grid-template-rows: 1.7fr 0.3fr;
+      grid-template-areas:
+        'S2Main'
+        'S2Buttons';
+    }
   }
 
   .S2Buttons {
     grid-area: S2Buttons;
     display: flex;
-    writing-mode: vertical-lr;
     cursor: pointer;
-    flex-direction: row;
-    transform: scale(-1);
-    width: 2.5vw;
+    font-family: $titleFont;
     p {
-      border: 1px solid #000000;
-      padding: 10px;
+      padding: 13px 4px;
       flex: 1 1 auto;
+      transition: 0.3s;
+      font-size: 26px;
+      &:hover {
+        background: linear-gradient(
+          to left,
+          #cc92fc03 0%,
+          #cc92fc0a 44%,
+          #cc92fc26 100%
+        );
+        color: #ffffffd4;
+      }
+    }
+    @media (min-aspect-ratio: 11/8) and (max-aspect-ratio: 32/15) {
+      writing-mode: vertical-lr;
+      flex-direction: row;
+      width: 2.5vw;
+      transform: scale(-1);
+      border-left: 1px solid $purple;
+    }
+    @media (min-aspect-ratio: 32/15), (max-aspect-ratio: 11/8) {
     }
   }
-
+  .selected {
+    transition: 0.3s;
+    background: linear-gradient(
+      to left,
+      #cc92fc03 0%,
+      #cc92fc0a 44%,
+      #cc92fc26 100%
+    );
+    color: #ffffffd4;
+  }
   .S2Main {
     display: grid;
     grid-template-columns: 1fr;
     grid-template-rows: 0.4fr 1.6fr;
-    gap: 0px 0px;
+    gap: 20px 0px;
     grid-auto-flow: row;
     grid-area: S2Main;
     place-items: center;
@@ -93,12 +159,40 @@
     grid-area: 1 / 1 / 2 / 2;
     display: grid;
     place-items: center;
+    width: 100%;
+    border-bottom: 1px solid $blue;
+    padding-bottom: 1.5vh;
+    gap: 4px 0px;
+    font-family: $titleFont;
   }
 
   .S2About {
+    padding-left: 1vw;
+    display: grid;
+    height: 100%;
+    grid-template-columns: 1fr;
+    grid-auto-rows: min-content;
+    gap: 2vh 0px;
+    grid-template-areas:
+      'AboutHeader'
+      'AboutContent'
+      'AboutSocials';
     grid-area: 2 / 1 / 3 / 2;
   }
-
+  .AboutHeader {
+    grid-area: AboutHeader;
+    font-family: $titleFont;
+    font-weight: 600;
+    font-size: 24px;
+  }
+  .AboutContent {
+    grid-area: AboutContent;
+    font-family: $textFont;
+    font-weight: 200;
+  }
+  .AboutSocials {
+    grid-area: AboutSocials;
+  }
   .S2Experience {
     grid-area: 2 / 1 / 3 / 2;
   }
@@ -118,21 +212,23 @@
   }
 
   .S2Name {
-    font-size: 22px;
+    font-size: 2em;
     font-weight: 700;
     text-align: center;
     white-space: nowrap;
     font-family: $titleFont;
+    color: white;
   }
 
   .S2Job {
     font-family: $textFont;
-    font-size: 11px;
+    font-size: 1em;
     white-space: nowrap;
-    font-weight: 500;
+    font-weight: 300;
     opacity: 0.7;
     text-transform: uppercase;
     letter-spacing: 1.5px;
     margin: 0;
+    color: $lightPurple;
   }
 </style>
