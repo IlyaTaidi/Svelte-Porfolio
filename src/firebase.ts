b00438b6d7b7b7e87b1df0879eee98c302887452
IlyaTaidi <65737081+IlyaTaidi@users.noreply.github.com>
2022-08-06 14:36:07 -0400

    build and deploy

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "Firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCz6fuct9FxXso93MPGnTmXUitWfpWKTcg",
+  authDomain: "svelteportfolio-bfe0d.firebaseapp.com",
+  projectId: "svelteportfolio-bfe0d",
+  storageBucket: "svelteportfolio-bfe0d.appspot.com",
+  messagingSenderId: "283055177781",
+  appId: "1:283055177781:web:c146b54deb65e6d675b17c",
+  measurementId: "G-5LHMPEWWZM"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+
+export { }; // 👈️ if you don't have anything else to export
